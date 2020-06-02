33040b2ba8b92feacc2ff9d0303804eb0309e08e
Claudio Escobar <claudioe0407@gmail.com>
2020-06-01 22:41:59 -0300

    typo

--- src/Query/Translator/MysqlTranslator.php
+++ src/Query/Translator/MysqlTranslator.php
@@ -5,5 +5,5 @@ namespace Esclaudio\Datatables\Query\Translator;
 class MysqlTranslator extends AnsiTranslator
 {
     protected $quotePrefix = '`';
-    protected $suffixPrefix = '`';
+    protected $quoteSuffix = '`';
 }
